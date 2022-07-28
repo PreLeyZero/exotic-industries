3b373abb102be8796cbb1a53649198f52e46ebdb
PreLeyZero <Paul.Zschoppe@gmx.de>
2022-07-28 17:28:31 +0200

    init

---
+++ build_mod.ps1
@@ -0,0 +1,134 @@
+# cd D:\exotic-industries\exotic-industries_0.0.1
+# powershell -ExecutionPolicy Bypass -File build_mod.ps1
+
+$param1 = $args[0]
+
+function Write-ColorOutput($ForegroundColor)
+{
+    # save the current color
+    $fc = $host.UI.RawUI.ForegroundColor
+
+    # set the new color
+    $host.UI.RawUI.ForegroundColor = $ForegroundColor
+
+    # output
+    if ($args) {
+        Write-Output $args
+    }
+    else {
+        $input | Write-Output
+    }
+
+    # restore the original color
+    $host.UI.RawUI.ForegroundColor = $fc
+}
+
+function make_both {
+    Write-ColorOutput green ("Building source mod...")
+
+    $FactorioFolder = "C:\Users\A\AppData\Roaming\Factorio\mods\"
+
+    # Find both mods in folder above
+
+    Write-ColorOutput darkgreen ("Searching files.")
+
+    $LocationPath = (Get-Location).path
+    $ParentPath = (get-item $LocationPath ).parent.FullName
+
+    Write-ColorOutput darkgreen ("Checking files.")
+
+    $SourceArray = Get-ChildItem -Path $ParentPath -Recurse | Where {$_.Name -match 'exotic-industries_'} | %{$_.FullName}
+    $GraphicalArray = Get-ChildItem -Path $ParentPath -Recurse | Where {$_.Name -match 'exotic-industries-graphics_'} | %{$_.FullName}
+
+    # --> kommt nicht hierhin
+
+    # check if both arrays only have one element, throw error otherwise
+
+    Write-ColorOutput darkgreen ("Done checking files.")
+
+    if ($SourceArray.count -eq 1) 
+    {
+        $SourceFolder = $SourceArray     
+    }
+    else
+    {
+        throw "There is more then one uniqe exotic-industries folder in this repository."
+    }
+
+    if ($GraphicalArray.count -eq 1) 
+    {
+        $GraphicalFolder = $GraphicalArray     
+    }
+    else
+    {
+        throw "There is more then one uniqe exotic-industries-graphics folder in this repository."
+    }
+
+    # Get new version numbers for source and graphic mod from info.json
+
+    Write-ColorOutput darkgreen ("Searching for version numbers.")
+
+    [string]$SourceVersion = (Get-Content (Join-Path -Path $SourceFolder -ChildPath "\info.json") -Raw | ConvertFrom-Json).version
+    [string]$GraphicalVersion = (Get-Content (Join-Path -Path $GraphicalFolder -ChildPath "\info.json") -Raw | ConvertFrom-Json).version
+
+    # Make targets with version numbers
+
+    $SourceTarget = Join-Path -Path $FactorioFolder -ChildPath (-join("exotic-industries_", $SourceVersion))
+    $GraphicalTarget = Join-Path -Path $FactorioFolder -ChildPath (-join("exotic-industries-graphics_", $GraphicalVersion))
+
+    # copy all updated filed using xcopy
+
+    Write-ColorOutput darkgreen ("Copying.")
+
+    xcopy $SourceFolder $SourceTarget /s /d /e /f /y /i
+    xcopy $GraphicalFolder $GraphicalTarget /s /d /e /f /y /i
+}
+
+function start_factorio {
+    # look if factorio is running, if so, restart it
+
+    $FactorioProcess = Get-Process factorio -ErrorAction SilentlyContinue
+    if($FactorioProcess -eq $null)
+    {
+        # start factorio
+        Write-ColorOutput blue ("Starting factorio.")
+        Start-Process -FilePath "D:\SteamLibrary\steamapps\common\Factorio\bin\x64\factorio.exe" -Wait
+    }
+    else
+    {   
+        Write-ColorOutput blue ("Restarting factorio.")
+
+        # be nice first
+        $FactorioProcess.CloseMainWindow()
+
+        # then kill it
+        Sleep 5
+        if (!$FactorioProcess.HasExited)
+        {
+            $FactorioProcess | Stop-Process -Force
+        }
+
+        # start factorio
+        Start-Process -FilePath "C:\Program Files (x86)\Steam\steamapps\common\Factorio\bin\x64\factorio.exe" -Wait
+    }
+}
+
+
+# select action
+switch ($param1)
+{
+    1 {
+        make_both
+        start_factorio
+    }
+
+    2 {
+        make_both
+    }
+
+    default {
+        Write-ColorOutput yellow ("False parameter")
+    }
+}
+
+exit 0
---
+++ other/convention.md
@@ -0,0 +1,37 @@
+# Mod structure
+
+Short for exotic-industries `ei_` will be used as identifier. All commonly used functions are packed under the lib folder into `ei_lib.`
+
+
+# Naming convention
+
+The following structure will be used for naming.
+Full names are separated by `-` and different sections by `_`
+
+- **Items:** `ei_gold-ingot`
+- **Recipes:** `ei_gold-ingot`
+
+there may be sub categories/additional variants
+or the recipe name will have additional info to the production way. The item named in a recipe should always be the product, if there is no product or multiple with no clear main product the machine name should be used instead. 
+
+- **Sub-Recipes:** `ei_gold-ingot:casting`
+- **Other Sub-Recipe:** `ei_gold-ingot:smelting`
+- **No clear main product:** `ei_fusion-reactor:fusing`
+- **Void recipe:** `ei_gold-ingot:void`
+
+Technologies and entities are named in the same way.
+
+- **Technology:** `ei_gold-ingot`
+- **Entity:** `ei_arc-furnace`
+
+Fluids are named similar, however there may also be different states, which use the same : convention.
+Molten metals and other liquid forms of items are also included using `liquid_`. There may be hot/cold versions without using the built in temperature system.
+
+- **Generic fluid:** `ei_dirty-water`
+- **Molten metal:** `ei_liquid_gold`
+- **Hot lead:** `ei_liquid_lead:hot`
+
+All other prototypes f.e. for subgroups and categories are named the same way. When a recipe category is only associated with one machine then the machine name will be used with a `crafting_` tag before. Categories for multiple entities like advanced-smelting use collective terms instead. 
+
+- **Crafting category for one machine:** `ei_crafting_arc-furnace`
+- **Collective terms:** `ei_advanced_crafting`
