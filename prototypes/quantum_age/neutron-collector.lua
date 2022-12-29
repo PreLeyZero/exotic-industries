35dd0e45717994106141395f2b5cb4d0621bad17
PreLeyZero <Paul.Zschoppe@gmx.de>
2022-12-29 20:21:58 +0100

    fixed crash with charged neutron container

--- prototypes/quantum_age/neutron-collector.lua
+++ prototypes/quantum_age/neutron-collector.lua
@@ -170,7 +170,7 @@ local base_recipe = {
     },
     enabled = true,
     hidden = true,
-    main_product = "ei_neutron-container",
+    main_product = "ei_charged-neutron-container",
 }
 
 -- make recipes for 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 percent efficiency
