7f70f9c3ff3b79f9385e76f14f79f7874cf3d6da
PreLeyZero <Paul.Zschoppe@gmx.de>
2023-03-19 19:48:28 +0100

    swap to v0.2.1

--- migrations/exotic-industries_0.2.1.lua
+++ migrations/exotic-industries_0.2.1.lua
@@ -5,4 +5,4 @@ for _, force in pairs(game.forces) do
     force.reset_technology_effects()
 end
 
-game.print("Exotic Industries 0.1.10 migration complete")
+game.print("Exotic Industries 0.2.1 migration complete")
