1322484e45244aa6a9fc79cb9dceb1d5f6057821
PreLeyZero <Paul.Zschoppe@gmx.de>
2023-04-16 13:26:43 +0200

    added migration

---
+++ migrations/exotic-industries_0.4.0.lua
@@ -0,0 +1,11 @@
+-- fix techs
+for _, force in pairs(game.forces) do
+    force.reset_recipes()
+    force.reset_technologies()
+    force.reset_technology_effects()
+end
+
+-- recalc new total tech count
+ei_tech_scaling.init()
+
+game.print("Exotic Industries 0.4.0 migration complete")
