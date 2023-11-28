fb0c650875464b9dd988d1d63acb6b5d3ff1d7e0
dtcameron <cameron.daniel.t@gmail.com>
2023-11-29 00:16:41 +0100

    feat(crystal generator): crystal gen now checks for running recipe of resonators

--- prototypes/alien_structures/crystal-generator.lua
+++ prototypes/alien_structures/crystal-generator.lua
@@ -13,7 +13,7 @@ data:extend({
         icon = ei_graphics_item_path.."combustion-turbine.png",
         icon_size = 64,
         subgroup = "energy",
-        order = "b[steam-power]-c[combustion-turbine]",
+        order = "b[steam-power]-c[crystal-generator]",
         place_result = "ei_crystal-generator",
         stack_size = 10
     },
@@ -64,6 +64,14 @@ data:extend({
         gui_mode = "none",
         max_power_output = "100TW",
         energy_production = '10kW',
+        radius_visualisation_specification = {
+            sprite = {
+                filename = ei_graphics_other_path.."radius.png",
+                width = 256,
+                height = 256
+            },
+            distance = 20
+        },
         animation = {
             filename = ei_graphics_entity_path.."combustion-turbine_animation.png",
             size = {512,512},
@@ -103,23 +111,22 @@ ei_data = require("lib/data")
 
 
 --====================================================================================================
---CRYSTAL GENERATOR
--- sin(x)^{18}*sin(2x-0.5)* 8 + cos(2x)/2
+--CRYSTAL RESONATOR
 --====================================================================================================
 
 data:extend({
     {
-        name = "ei_crystal-generator",
+        name = "ei_crystal-resonator",
         type = "item",
-        icon = ei_graphics_item_path.."combustion-turbine.png",
+        icon = ei_graphics_item_path.."alien-beacon.png",
         icon_size = 64,
         subgroup = "energy",
-        order = "b[steam-power]-c[combustion-turbine]",
-        place_result = "ei_crystal-generator",
+        order = "b[steam-power]-c[crystal-resonator]",
+        place_result = "ei_crystal-resonator",
         stack_size = 10
     },
     {
-        name = "ei_crystal-generator",
+        name = "ei_crystal-resonator",
         type = "recipe",
         category = "crafting",
         energy_required = 10,
@@ -132,69 +139,109 @@ data:extend({
             {"steam-engine", 4},
             {"concrete", 40}
         },
-        result = "ei_crystal-generator",
+        result = "ei_crystal-resonator",
         result_count = 1,
         enabled = false,
         always_show_made_in = true,
-        main_product = "ei_crystal-generator",
+        main_product = "ei_crystal-resonator",
     },
     {
-        name = "ei_crystal-generator",
-        type = "electric-energy-interface",
-        icon = ei_graphics_item_path.."combustion-turbine.png",
+        name = "ei_crystal-resonator",
+        type = "recipe-category",
+    },
+    {
+        name = "ei_crystal-resonator",
+        type = "assembling-machine",
+        result_inventory_size = 0,
+        source_inventory_size = 1,
+        crafting_categories = {"ei_crystal-resonator"},
+        icon = ei_graphics_item_path.."alien-beacon.png",
         icon_size = 64,
         flags = {"placeable-neutral", "placeable-player", "player-creation"},
         minable = {
             mining_time = 1,
-            result = "ei_crystal-generator"
+            result = "ei_crystal-resonator"
         },
         max_health = 300,
         corpse = "big-remnants",
         dying_explosion = "medium-explosion",
-        collision_box = {{-4.2, -4.3}, {4.2, 4.3}},
-        selection_box = {{-4.3, -4.5}, {4.3, 4.5}},
+        collision_box = {{-2.2, -2.2}, {2.2, 2.2}},
+        selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
         map_color = ei_data.colors.assembler,
+        crafting_speed =1,
         energy_source = {
-            type = "electric",
-            usage_priority = "primary-output",
-            input_flow_limit = "2YW",
-            output_flow_limit =  "20YW",
-            buffer_capacity = "100MJ",
-            render_no_power_icon = false
-          },
-        gui_mode = "none",
-        max_power_output = "100TW",
-        energy_production = '10kW',
+            type = 'electric',
+            usage_priority = 'primary-input',
+        },
+        energy_usage = '10MW',
+        fluid_boxes = {
+            {   
+                base_area = 1,
+                base_level = -1,
+                height = 2,
+                pipe_covers = pipecoverspictures(),
+                pipe_picture = ei_pipe_big,
+                pipe_connections = {
+                    {type = "input", position = {3, 0}},
+                },
+                production_type = "input",
+            },
+        },
+        
         animation = {
-            filename = ei_graphics_entity_path.."combustion-turbine_animation.png",
+            filename = ei_graphics_entity_path.."alien-beacon_animation.png",
             size = {512,512},
-            width = 512,
-            height = 512,
-            shift = {0,-0.2},
-	        scale = 0.65,
+            shift = {0, 0.5},
+	        scale = 0.35,
             line_length = 4,
             lines_per_file = 4,
             frame_count = 16,
-            animation_speed = 0.2,
+            animation_speed = 0.4,
+            run_mode = "backward",
         },
-        idle_animation = {
-            filename = ei_graphics_entity_path.."combustion-turbine.png",
-            size = {512,512},
-            width = 512,
-            height = 512,
-            shift = {0,-0.2},
-	        scale = 0.65,
-            line_length = 1,
-            -- lines_per_file = 2,
-            frame_count = 1,
-            animation_speed = 0.2,
-            repeat_count = 16,
+        radius_visualisation_specification = {
+            sprite = {
+                filename = ei_graphics_other_path.."radius.png",
+                width = 256,
+                height = 256
+            },
+            distance = 20
         },
-        working_sound =
-        {
-            sound = {filename = "__base__/sound/steam-turbine.ogg", volume = 0.2},
-            apparent_volume = 0.3,
+    },
+    -- Crystal resonator recipes
+    {
+        name = "ei_crystal_resonator:basic",
+        type = "recipe",
+        category = "ei_crystal-resonator",
+        energy_required = 1,
+        ingredients = {
+            {type = "item", name = "ei_energy-crystal", amount = 1},
         },
+        results = {},
+        result_count = 1,
+        enabled = false,
+        hidden = true,
+        icon = ei_graphics_other_path.."energy-injector-pylon.png",
+        icon_size = 64,
+        subgroup = "ei_black-hole",
+        order = "b-a",
+    },
+    {
+        name = "ei_crystal_resonator:overclocked",
+        type = "recipe",
+        category = "ei_crystal-resonator",
+        energy_required = 1,
+        ingredients = {
+            {type = "item", name = "ei_energy-crystal", amount = 10},
+        },
+        results = {},
+        result_count = 1,
+        enabled = false,
+        hidden = true,
+        icon = ei_graphics_other_path.."energy-injector-pylon.png",
+        icon_size = 64,
+        subgroup = "ei_black-hole",
+        order = "b-a",
     },
 })
 
--- scripts/control/crystal_generator.lua
+++ scripts/control/crystal_generator.lua
@@ -1,4 +1,11 @@
 local model = {}
+local resonator_recipe_to_effect = {
+    ["nil"] = 0,
+    ["ei_crystal_resonator:basic"] = 1,
+    ["ei_crystal_resonator:overclocked"] = 2,
+     
+}
+
 
 --====================================================================================================
 --CRYSTAL GENERATOR
@@ -16,6 +23,7 @@ function model.entity_check(entity)
         return false
     end
 
+
     return true
 end
 
@@ -32,6 +40,30 @@ function model.check_init(id)
     if not global.ei.crystal_generator[id] then
         global.ei.crystal_generator[id] = {}
     end
+end
+
+function model.get_resonator_efficiency(generator, resonator)
+    if resonator.crafting_progress > 0 and resonator.energy > 1000 then
+        local resonator_effect = 1
+        -- check for recipe 
+        resonator_effect = resonator_effect * resonator_recipe_to_effect[resonator.get_recipe().name]
+        local check_position = {
+            x = generator.position.x + (generator.position.x - resonator.position.x ),
+            y = generator.position.y + (generator.position.y - resonator.position.y ) 
+        }
+        local partner_resonator = generator.surface.find_entities_filtered{
+            name = "ei_crystal-resonator",
+            position = check_position,
+            radius = 0.1
+        }
+        if next(partner_resonator) == nil then
+            resonator_effect = resonator_effect * -1   
+        end 
+        return resonator_effect
+    else 
+        return 0
+    end
+
 end
 
 --UPDATE RELATED
@@ -56,6 +88,18 @@ end
 function model.update_crystal_generator(unit)
 
     local entity = global.ei.crystal_generator[unit].entity
+    local resonators = global.ei.crystal_generator[unit].resonators
+
+    if not resonators then 
+        return 
+    end
+
+    local resonator_effect = 0 
+    for _, resonator in pairs(resonators) do
+        resonator_effect = resonator_effect + model.get_resonator_efficiency(entity, resonator)
+    end
+
+    -- we need to check all nearby assembly machines
 
     if not model.entity_check(entity) then
         return
@@ -68,13 +112,8 @@ function model.update_crystal_generator(unit)
 
     local power = math.sin(x)^18 * math.sin(2 * x - 0.5) * 8 + math.cos(2 * x)
     local mega = 1000000
-    entity.power_production = (power + 1.282) * mega
-    -- if power > 0 then
-    --     entity.power_usage = 0
-    -- elseif power <= 0 then 
-    --     entity.power_production = 0
-    --     entity.power_usage = power * mega
-    -- end
+    entity.power_production = (power + 1.282) * mega * resonator_effect
+
 
 
 end
@@ -99,8 +138,20 @@ function model.register_crystal_generator(entity)
 
     model.check_init(entity.unit_number)
 
-    -- register this black hole
+    -- register this crystal generator
     global.ei.crystal_generator[entity.unit_number].entity = entity
+
+    local entities = entity.surface.find_entities_filtered{
+        name = "ei_crystal-resonator",
+        position = entity.position,
+        radius = 20,
+    }
+    global.ei.crystal_generator[entity.unit_number].resonators = {}
+    if entities ~= nil then 
+        for i, resonator in pairs(entities) do 
+            global.ei.crystal_generator[entity.unit_number].resonators[resonator.unit_number] = resonator
+        end 
+    end
 
 
 end
@@ -114,9 +165,57 @@ function model.unregister_crystal_generator(entity)
 
     model.check_init()
 
-    -- unregister this black hole
+    -- unregister this crystal generator
     global.ei.crystal_generator[entity.unit_number] = nil
 
+end
+
+
+function model.register_crystal_resonator(entity)
+
+    if entity.name ~= "ei_crystal-resonator" then
+        return
+    end
+
+    model.check_init(entity.unit_number)
+
+    -- register this crystal generator
+    -- global.ei.crystal_generator[entity.unit_number].entity = entity
+
+    local entities = entity.surface.find_entities_filtered{
+        name = "ei_crystal-generator",
+        position = entity.position,
+        radius = 20,
+    }
+    local nearby_generator = entities[1]
+    if (nearby_generator ~= nil) then 
+        global.ei.crystal_generator[nearby_generator.unit_number].resonators[entity.unit_number] = entity
+    end
+
+
+end
+
+
+function model.unregister_crystal_resonator(entity)
+
+    if entity.name ~= "ei_crystal-resonator" then
+        return
+    end
+
+    model.check_init()
+
+    -- unregister this crystal generator
+    -- global.ei.crystal_generator[entity.unit_number] = nil
+
+    local entities = entity.surface.find_entities_filtered{
+        name = "ei_crystal-generator",
+        position = entity.position,
+        radius = 20,
+    }
+    local nearby_generator = entities[1]
+    if (nearby_generator ~= nil) then 
+        global.ei.crystal_generator[nearby_generator.unit_number].resonators[entity.unit_number] = nil
+    end
 end
 
 
@@ -133,6 +232,7 @@ function model.on_built_entity(entity)
     end
 
     model.register_crystal_generator(entity)
+    model.register_crystal_resonator(entity)
 
 end
 
@@ -144,6 +244,7 @@ function model.on_destroyed_entity(entity)
     end
 
     model.unregister_crystal_generator(entity)
+    model.unregister_crystal_resonator(entity)
 end
 
 
@@ -160,3 +261,15 @@ end
 ------------------------------------------------------------------------------------------------------
 
 return model
+
+
+-- todo:
+
+-- create child list
+
+-- when child placed, add it to list of children pairs and calculate the pair effiency
+-- when child destroyed, remove it from the list of children pair and calulcate pair efficency
+
+-- when parent destroyed, clear child list?
+
+-- on parent tick, check to see if children are crafting, if they are then set power level
