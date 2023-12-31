684d8d631398483ec92a45901785b9cd54a9167a
PreLeyZero <52599424+PreLeyZero@users.noreply.github.com>
2023-12-31 13:27:12 +0100

    update to v0.5

---
+++ migrations/exotic-industries_0.5.0.lua
@@ -0,0 +1,15 @@
+local ei_tech_scaling = require("scripts/control/tech_scaling")
+
+-- fix techs
+for _, force in pairs(game.forces) do
+    force.reset_recipes()
+    force.reset_technologies()
+    force.reset_technology_effects()
+end
+
+-- recalc new total tech count
+ei_tech_scaling.init()
+
+game.print("Exotic Industries 0.5.0 migration complete")
+
+game.print({"exotic-industries.alien-migration-complete"})
