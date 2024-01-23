78bd18843a1a34681c419e053383bd9e8332aa7d
PreLeyZero <52599424+PreLeyZero@users.noreply.github.com>
2024-01-24 00:34:01 +0100

    v0.5.8

--- scripts/data-updates/locale_updates.lua
+++ scripts/data-updates/locale_updates.lua
@@ -2,15 +2,45 @@
 --LOCALE UPDATES
 --====================================================================================================
 
--- manual updates
--- [key] = source, value = target
-local to_mirror = {
-    --["ei_deep-drill"] = "ei_advanced-deep-drill",
-}
 
-for i,v in pairs(to_mirror) do
-    data.raw["item"][v].localised_description = data.raw["item"][tostring(i)].localised_description
+-- get list of all fluids with fuel value
+local fluids = {}
+local exclude = {
+    ["ei_drill-fluid"] = true,
+    ["ei_liquid-nitrogen"] = true,
+    ["ei_critical-steam"] = true
+}
+local max_chars = 200 - 25
+
+for fluid_name, fluid in pairs(data.raw["fluid"]) do
+    if fluid.fuel_value then
+        if exclude[fluid_name] then goto continue end
+
+        table.insert(fluids, fluid_name)
+    end
+    ::continue::
 end
+local icon_table = ""
+-- make list of all fluid icons
+for _, fluid_name in ipairs(fluids) do
+    new_icon_table = icon_table.."[fluid="..fluid_name.."]"
+    if string.len(new_icon_table) > max_chars then
+        break
+    end
+    icon_table = new_icon_table
+
+end
+
+data.raw["item"]["ei_fluid-boiler"].localised_description = {
+    "",
+    {"item-description.ei_fluid-boiler"},
+    icon_table
+}
+data.raw["item"]["ei_fluid-heater"].localised_description = {
+    "",
+    {"item-description.ei_fluid-heater"},
+    icon_table
+}
 
 -- most entities of EI and also vanilla dont have their own description, just their items
 -- therefore align them
@@ -32,15 +62,17 @@ for item_name, item in pairs(data.raw["item"]) do
         for i,v in ipairs(possible_types) do
             if data.raw[v][item.place_result] then
                 entity = data.raw[v][item.place_result]
+
+                if not entity then goto continue end
+                if not item.localised_description then goto continue end
+                -- if entity.localised_description then goto continue end
+                data.raw[v][item.place_result].localised_description = item.localised_description
+
+                log("copied description from item "..item_name.." to entity "..item.place_result)
+
+                goto continue
             end
         end
-
-        if not entity then goto continue end
-        if not item.localised_description then goto continue end
-        if entity.localised_description then goto continue end
-
-        entity.localised_description = table.deepcopy(item.localised_description)
-        log("Mirrored locale description from "..item_name.." (item) to "..entity.name.." (entity)")
 
         ::continue::
 
